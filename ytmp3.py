85be94c609a50d868e8555e2aeeef85414d9a6d3
Dandier <61583533+dandiers@users.noreply.github.com>
2023-12-29 01:08:10 +0700

    Update ytmp3.py

--- ytmp3.py
+++ ytmp3.py
@@ -1,16 +1,23 @@
 import os
 import pytz
-import datetime
 from pystyle import *
 from pytube import YouTube
-from datetime import datetime, timedelta
+from datetime import datetime
+from tqdm import tqdm
 
 def banner():
-        os.system('cls' if os.name == 'nt' else 'clear')    
-        title = f"""
+    os.system('cls' if os.name == 'nt' else 'clear')    
+    title = f"""
+         
+         
+         
+         
+         
+         
+                 
          
         
-                                           {datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%A, %d %B %Y %H:%M WIB")}  
+               {datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%A, %d %B %Y %H:%M WIB")}  
     
 ▓██   ██▓▄▄▄█████▓ ███▄ ▄███▓ ██▓███     ▄▄▄█████▓ ██░ ██  ██▀███  ▓█████ ▓█████ 
  ▒██  ██▒▓  ██▒ ▓▒▓██▒▀█▀ ██▒▓██░  ██▒   ▓  ██▒ ▓▒▓██░ ██▒▓██ ▒ ██▒▓█   ▀ ▓█   ▀ 
@@ -27,17 +34,18 @@ def banner():
  Script ini di buat untuk mendownload youtube video mp3.
  Jika ingin mengubah script sebaiknya izin dandier.
    """
-        print(Colorate.Vertical(Colors.DynamicMIX((Col.red, Col.purple)), Center.XCenter(title if os.name == "nt" else title)))
-        
+    print(Colorate.Vertical(Colors.DynamicMIX((Col.red, Col.purple)), Center.XCenter(title if os.name == "nt" else title)))
 
 if __name__ == '__main__':
     banner()
-    video_url = input(f"{Col.pink}      video url:{Col.white} ")
+    video_url = input(f"{Col.pink}video url:{Col.white} ")
     if video_url.startswith(("http://", "https://")):
         yt = YouTube(video_url)
         audio_stream = yt.streams.filter(only_audio=True).first()
-        audio_stream.download()
-        os.rename(f"{audio_stream.title}.{audio_stream.subtype}", f"{audio_stream.title}.mp3")
-        print(Colorate.Vertical(Colors.DynamicMIX((Col.green, Col.green)), Center.XCenter(f"\nBerhasil Download {audio_stream.title}.mp3")))
+        print(Colorate.Vertical(Colors.DynamicMIX((Col.green, Col.green)), Center.XCenter(f"\nTitle: {audio_stream.title}.mp3")))
+        audio_stream.download(filename='tmp_file.mp3', skip_existing=True)
+        os.rename('tmp_file.mp3', f"{audio_stream.title}.mp3")
+        print(Colorate.Vertical(Colors.DynamicMIX((Col.green, Col.green)), Center.XCenter(f"\nSuccessfully Downloaded {audio_stream.title}.mp3")))
     else:
-        print(Colorate.Vertical(Colors.DynamicMIX((Col.red, Col.red)), Center.XCenter("Video Url Harus Berawalan https")))
+        print(Colorate.Vertical(Colors.DynamicMIX((Col.red, Col.red)), Center.XCenter("Video URL must start with https")))
+            
